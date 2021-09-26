345b4c5d6d395ef2cf207d6d0a17b0a27bb6b734
Schramm9 <schrammcj@gmail.com>
2021-09-26 18:47:28 -0400

    Initial commit

---
+++ 2020scbDataExplore.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 26 12:59:54 2021
+
+@author: Chris
+"""
+
+import pandas as pd
+import xml.etree.ElementTree as et
+
+def parse_XML(xml_file, df_cols): 
+    """Thanks to Roberto Preste Author From XML to Pandas dataframes,
+    from xtree ... to return out_df
+    https://medium.com/@robertopreste/from-xml-to-pandas-dataframes-9292980b1c1c
+    """
+    
+    xtree = et.parse(xml_file)
+    xroot = xtree.getroot()
+    rows = []
+    
+    for node in xroot: 
+        res = []
+        res.append(node.attrib.get(df_cols[0]))
+        for el in df_cols[1:]: 
+            if node is not None and node.find(el) is not None:
+                res.append(node.find(el).text)
+            else: 
+                res.append(None)
+        rows.append({df_cols[i]: res[i] 
+                     for i, _ in enumerate(df_cols)})
+    
+    out_df = pd.DataFrame(rows, columns=df_cols)
+        
+    return out_df
+
+data=parse_XML("2020SC_ElementData.xml", ["FHWAED", "STATE", "STRUCNUM", "EN", "EPN", "TOTALQTY", "CS1", "CS2", "CS3", "CS4"])
+

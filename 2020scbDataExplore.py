7eb11c140eea3794e7d706355c30bcdb73b2e868
Schramm9 <schrammcj@gmail.com>
2021-10-11 20:48:57 -0400

    second commit with .spydata file included

--- 2020scbDataExplore.py
+++ 2020scbDataExplore.py
@@ -33,5 +33,36 @@ def parse_XML(xml_file, df_cols):
         
     return out_df
 
-data=parse_XML("2020SC_ElementData.xml", ["FHWAED", "STATE", "STRUCNUM", "EN", "EPN", "TOTALQTY", "CS1", "CS2", "CS3", "CS4"])
+df2020=parse_XML("2020SC_ElementData.xml", ["FHWAED", "STATE", "STRUCNUM", "EN", "EPN", "TOTALQTY", "CS1", "CS2", "CS3", "CS4"])
+
+df2019=parse_XML("2019SC_ElementData.xml", ["FHWAED", "STATE", "STRUCNUM", "EN", "EPN", "TOTALQTY", "CS1", "CS2", "CS3", "CS4"])
+
+"""End Roberto Preste code From XML to Pandas dataframes,
+    from xtree ... to return out_df
+    https://medium.com/@robertopreste/from-xml-to-pandas-dataframes-9292980b1c1c
+"""
+
+df2020.index
+
+df2020.dtypes
+
+df2020.head
+
+""" df20_19 is a comparison of the data for the bridge conditions of year 2020 and 2019 """
+
+df20_19 = pd.merge(df2020, df2019, suffixes=['_20', '_19'], on=['STRUCNUM','EN'])
+
+df20_19.head
+
+df20_19a = pd.merge(df2020, df2019, on='EN', how='left')
+
+df2020.keys()    
+
+"""grouped_obj = df2020.groupby(["STRUCNUM"])
+for key, item in grouped_obj:
+    print("Key is: " + str(key))
+    print(str(item), "\n\n")"""
+    
+    
+    
 
